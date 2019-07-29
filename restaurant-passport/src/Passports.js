c3a9292d71573514b663c29c6d06dc9c59637e78
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-07-29 18:15:47 -0400

    made and rendered passports dummy data

--- restaurant-passport/src/Passports.js
+++ restaurant-passport/src/Passports.js
@@ -2,7 +2,26 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from "axios";
 
+const dummyData  = [
+  {
+    id: 0,
+    city: 'New York, NY',
+    restaurantCount: 0,
+  },
+  {
+    id: 1,
+    city: 'San Fransico, CA',
+    restaurantCount: 0,
+  },
+  {
+    id: 2,
+    city: 'Seattle, WA',
+    restaurantCount: 0,
+  },
+];
+
 const Passports = () => {
+  const [activePassportId, setactivePassportId] = useState();
   const [passports, setPassports] = useState();
 
   const retrievePassports = () => {
@@ -10,7 +29,6 @@ const Passports = () => {
       try {
         const repsonse = await axios.get('https://rickandmortyapi.com/api/episode/');
 
-        console.log(repsonse);
         setPassports(repsonse.data);
       } catch (error) {
         console.error(error);
@@ -21,6 +39,24 @@ const Passports = () => {
 
   return (
     <div>
+      {console.log(passports)}
+      <h2>Passports</h2>
+      <h3>Current: {dummyData.find((passport) => passport.id === activePassportId)}</h3>
+      <div className="Passports">
+        <div className="add-passport">
+          {/* <img src="" alt=""></img> // uncomment once you have an svg for a plus sign */}
+          <h3>+</h3>
+          <p>Add Passport</p>
+        </div>
+        {dummyData.map((passport) => {
+          return (
+            <div className="passport">
+              <h3>{passport.city}</h3>
+              <p>{passport.restaurantCount}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
