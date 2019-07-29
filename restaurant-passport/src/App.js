2137a29f643b2d97ed3c058942a5cc5448fe6623
Kizmet <bryantpatton@gmail.com>
2019-07-29 16:47:05 -0700

    basic passport layout with search function

--- restaurant-passport/src/App.js
+++ restaurant-passport/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import Passport from './components/Passport';
+
 
 function App() {
-	return <div className='App' />;
+	return (
+    <div className='App'>
+    <Passport />
+    </div>
+
+    );
 }
 
 export default App;
