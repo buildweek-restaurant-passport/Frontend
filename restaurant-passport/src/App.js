a62c6db2dd372b8924e311fdbb22b51640325732
Armando Roman <armandoroman1016@gmail.com>
2019-07-30 13:45:40 -0700

    navbar set up with routes logout function still needed

--- restaurant-passport/src/App.js
+++ restaurant-passport/src/App.js
@@ -4,15 +4,19 @@ import Passport from './components/Passport';
 import Passports from './Passports';
 import Register from './components/Register';
 import Login from './components/Login';
-import { Route } from 'react-router-dom';
+import { Route , Switch} from 'react-router-dom';
+import NavBar from './components/nav-bar/nav-bar'
 
 function App() {
 	return (
 		<div className='App'>
-			<Route exact path='/' component={Register} />
-			<Route exact path='/login' component={Login} />
-			<Route exact path='/passports' component={Passports} />
-			<Route exact path='/restaurants' component={Passport} />
+			<NavBar />
+				<Switch>
+					<Route exact path='/' component={Register} />
+					<Route exact path='/login' component={Login} />
+					<Route exact path='/passports' component={Passports} />
+					<Route exact path='/restaurants' component={Passport} />
+				</Switch>
 		</div>
 	);
 }
