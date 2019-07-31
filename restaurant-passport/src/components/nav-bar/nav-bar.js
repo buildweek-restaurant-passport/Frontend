22d8a9568d6bc24abf2ac096a102e0f6744bb91a
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-07-31 14:51:15 -0400

    conditional rendering of navbar

--- restaurant-passport/src/components/nav-bar/nav-bar.js
+++ restaurant-passport/src/components/nav-bar/nav-bar.js
@@ -3,21 +3,29 @@ import { Link } from 'react-router-dom';
 import logo from '../../assests/images/logo.png'
 
 const NavBar = (props) => {
-    console.log(props);
+    const isOnLandingPage = () => {
+        const landingPageUrls = [
+            '/',
+            '/login',
+            '/signup',
+        ];
+        return (landingPageUrls.some(url => url === props.location.pathname));
+    };
     
 
     return (
-
         <div className = 'nav-bar' style = {{display:'flex' , height: '90px', alignItems: 'center', justifyContent: 'space-between',margin: '10px 100px'}}>
-    
-            <div className = 'logo-container' style = {{height:'90px', width: 'auto'}}>
-                <img src = {logo} alt = 'restaurant passport logo' style = {{height:'100%', width: 'auto'}}></img>
-            </div>
-            <div className = 'nav-links' style = {{width:'44%', color: '#085f63', fontSize:'20px', display:'flex', justifyContent: 'space-between', fontWeight: '800',}}>
-                <Link to ='/passports'>My Passports</Link>
-                <Link to ='/restaurants'>Restaurants</Link> 
-                {props.location.pathname !== '/' && <Link to ='/'>Log Out</Link>}
-            </div>
+        
+                <div className = 'logo-container' style = {{height:'90px', width: 'auto'}}>
+                    <img src = {logo} alt = 'restaurant passport logo' style = {{height:'100%', width: 'auto'}}></img>
+                </div>
+            {!isOnLandingPage()
+                && <div className = 'nav-links' style = {{width:'44%', color: '#085f63', fontSize:'20px', display:'flex', justifyContent: 'space-between', fontWeight: '800',}}>
+                    {props.location.pathname !== '/passports' && <Link to ='/passports'>My Passport</Link>}
+                    <Link to ='/restaurants'>Restaurants</Link> 
+                    <Link to ='/'>Log Out</Link>
+                </div>
+            }
         </div>
     )
 }
