7a5b4601bd714d931212b35ddd8600791d3455f7
Justin Trombley <jtrombley96@gmail.com>
2019-07-31 19:03:43 -0400

    Signup/Login Styled

--- restaurant-passport/src/App.js
+++ restaurant-passport/src/App.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-
-import './App.css';
-import './components/passport-restaurants/passport.scss';
-import './components/access/access.scss';
-
 import Passport from './components/passport-restaurants/Passport.js';
 import Passports from './components/Passports';
 import Register from './components/access/Register';
@@ -12,14 +7,18 @@ import Login from './components/access/Login';
 import NavBar from './components/nav-bar/nav-bar';
 import Landing from './components/access/Landing';
 
+import './App.css';
+import './components/passport-restaurants/passport.scss';
+import './components/access/access.scss';
+
 function App() {
 	return (
-		<div className="App">
-			<Route path="/" component={NavBar} />
+		<div className='App'>
+			<Route path='/' component={NavBar} />
 			<Switch>
-				<Route exact path="/(|login|register)" component={Landing} />
-				<Route exact path="/passports" component={Passports} />
-				<Route exact path="/restaurants" component={Passport} />
+				<Route exact path='/(|login|register)' component={Landing} />
+				<Route exact path='/passports' component={Passports} />
+				<Route exact path='/restaurants' component={Passport} />
 			</Switch>
 		</div>
 	);
