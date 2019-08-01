238e6420dff119353cb8bb01ed1692b87982c8a2
Justin Trombley <jtrombley96@gmail.com>
2019-08-01 13:24:29 -0400

    bug fix

--- restaurant-passport/src/App.js
+++ restaurant-passport/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-<<<<<<< HEAD
 
 import './App.css';
 import './components/restaurant-list/passport.scss';
 import './components/access/access.scss';
 
 import Passport from './components/restaurant-list/Passport.js';
-=======
-import Passport from './components/passport-restaurants/Passport.js';
->>>>>>> 94442d78cd50bfc34b2feaa39735cfcf250d0e74
 import Passports from './components/Passports';
 import Register from './components/access/Register';
 import Login from './components/access/Login';
 import NavBar from './components/nav-bar/nav-bar';
 import Landing from './components/access/Landing';
 
-import './App.css';
-import './components/passport-restaurants/passport.scss';
-import './components/access/access.scss';
 
 function App() {
 	return (
