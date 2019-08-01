286a79ea07a2b499d2e7601371aa4c73f3624549
patrick baldwin <patrickbaldwin614@gmail.com>
2019-08-01 15:45:58 -0400

    passport image is now a local copy

--- restaurant-passport/src/components/Passports.js
+++ restaurant-passport/src/components/Passports.js
@@ -3,6 +3,8 @@ import { Header, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import './access/access.scss';
 
+import passportImage from "../assests/images/passsport.jpg";
+
 const Passports = () => {
 	return (
 		<div>
@@ -10,7 +12,7 @@ const Passports = () => {
 				My Passport
 			</Header>
 			<Image
-				src='https://media.afar.com/uploads/images/afar_post_headers/images/wsGsA768bP/original_shutterstock_1110443648.jpg?1543263550'
+				src={passportImage}
 				size='large'
 				circular
 				centered
